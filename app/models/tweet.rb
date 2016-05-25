17b0e61a5b55fe00ed1b0903ab1060be06f9ced1
Kenneth Bergquist <bergquist.kenneth@gmail.com>
2016-05-24 23:36:37 -0400

    only english tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,15 +5,15 @@ class Tweet < ActiveRecord::Base
   def self.refresh_tweets
     Tweet.delete_all
 
-    $twitter.search('healthcare', result_type: :recent).take(15).each{|tweet| Tweet.create!(text: tweet.text, topic: 'healthcare',
+    $twitter.search('healthcare', result_type: :recent, lang: "en").take(15).each{|tweet| Tweet.create!(text: tweet.text, topic: 'healthcare',
                                                  source: tweet.source, retweet_count: tweet.retweet_count,
                                                  favorite_count: tweet.favorite_count, user: tweet.user.name)}
 
-    $twitter.search('nasa', result_type: :recent).take(15).each{|tweet| Tweet.create!(text: tweet.text, topic: 'nasa',
+    $twitter.search('nasa', result_type: :recent, lang: "en").take(15).each{|tweet| Tweet.create!(text: tweet.text, topic: 'nasa',
                                                  source: tweet.source, retweet_count: tweet.retweet_count,
                                                  favorite_count: tweet.favorite_count, user: tweet.user.name)}
 
-    $twitter.search('open source', result_type: :recent).take(15).each{|tweet| Tweet.create!(text: tweet.text, topic: 'open_source',
+    $twitter.search('open source', result_type: :recent, lang: "en").take(15).each{|tweet| Tweet.create!(text: tweet.text, topic: 'open_source',
                                                  source: tweet.source, retweet_count: tweet.retweet_count,
                                                  favorite_count: tweet.favorite_count, user: tweet.user.name)}
   end
